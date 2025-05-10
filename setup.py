270585a3b740388866c824640b1fee7baa0cc78f
Bharti1004 <bj163431@gmail.com>
2025-05-10 13:41:27 +0530

    folder structure designed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Bharti Jain',
+    author_email='bhartijain033@gmail.com',
+    packages=find_packages(),  # it find the _init_.py , when it finds it consider it local
+    install_requires=[]
+)
